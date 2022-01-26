1eefea9a968209200bc6349d92c2c1fffa4d5f2e
Artem Nikolaev <artem@nikolaev.by>
2022-01-26 16:53:56 +0300

    Initial Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+#### Tutorial
+# [WebGL2 Fundamentals](https://webgl2fundamentals.org/)
+
+##Desctiption
+Examples from tutorial
---
+++ utils/resize-canvas.js
@@ -0,0 +1,10 @@
+function resizeCanvas(canvas) {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+
+    window.addEventListener('resize', () => {
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
+
+    })
+}
